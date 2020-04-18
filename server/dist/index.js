68c73b15a52cf5674d4ba4809f00f644d4575adc
Atchuta rama rao Rayudu <atchutaramaraorayudu@Atchutas-MacBook-Pro.local>
2020-04-18 02:57:09 -0700

    Adding root package json file

--- server/dist/index.js
+++ server/dist/index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const http_1 = __importDefault(require("http"));
+const path_1 = __importDefault(require("path"));
 const sockets_1 = __importDefault(require("./sockets"));
 require("./config/index");
 const app = express_1.default();
@@ -13,8 +14,9 @@ const httpServer = http_1.default.createServer(app);
 const PORT = process.env.PORT || 5000;
 const io = sockets_1.default(httpServer);
 app.use(cors_1.default());
+app.use(express_1.default.static(path_1.default.join(__dirname, '..', '..', 'client-app', 'build')));
 app.get("/", (req, res) => {
-    res.send('11111');
+    res.sendFile(path_1.default.join(__dirname, '..', '..', 'client-app', 'build', 'index.html'));
 });
 httpServer.listen(PORT, () => {
     console.log('Listening on port', PORT);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import http from 'http';
+import path from 'path';
 import SocketClient from './sockets';
 
 import './config/index';
@@ -11,9 +12,10 @@ const PORT = process.env.PORT || 5000;
 const io = SocketClient(httpServer);
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, '..',  '..', 'client-app','build')));
 
 app.get("/", (req, res) => {
-    res.send('11111');
+    res.sendFile(path.join(__dirname, '..', '..', 'client-app','build', 'index.html'));
 })
 
 httpServer.listen(PORT, () => {
